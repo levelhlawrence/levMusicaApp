8ea8d59809fa2518fd9f6d5eedc3f1f3d5faef47
LawrenceLevel-FS <levelhlawrence@gmail.com>
2024-10-05 11:57:28 -0400

    updated documentation and callback

--- server/index.js
+++ server/index.js
@@ -1,4 +1,19 @@
-const { app, port } = require("./app/server");
+const app = require("./app/server");
+//PORT
+const port = process.env.PORT || 3001;
+
+const swagger = require("express-jsdoc-swagger");
+
+// Live Documentation
+swagger(app)({
+  info: {
+    title: "LevMusica",
+    version: "1.0.0",
+    description: "Music app inspired by the Spotify API .",
+  },
+  baseDir: "./app/router",
+  swaggerUIPath: "/",
+});
 
 // Running server
 app.listen(port, () => {
